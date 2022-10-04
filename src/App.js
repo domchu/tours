e23c0ef42242611cd0664acd57c7f6d4ebc392a0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-05 00:38:26 +0100

    created the loading & tour comp then import it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Loading from "./Loading";
+import Tour from "./Tour";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>React Tours project</h2>
+    </div>
+  );
 }
 
 export default App;
