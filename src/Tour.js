fcb5a38007e8526fe6dcbfcf8a6030d82fc8d992
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-05 11:27:49 +0100

    chore: conditional rendering of read more toggle

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price }) => {
+  //read more text
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +12,16 @@ const Tour = ({ id, name, info, image, price }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+
+        {/* the read more and show less toggle is done by conditional
+        rendering set the number of words from zero to (desire) */}
+
+        <p>
+          {readMore ? info : `${info.substring(0, 200)} ...`}{" "}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read more"}
+          </button>
+        </p>
         <button className="delete-btn">Not interested</button>
       </footer>
     </article>
