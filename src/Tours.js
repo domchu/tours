6bcadea0de29d5651383f4560eef6eba24c129e2
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-05 02:10:13 +0100

    chore: mapping the tour component to the dom

--- src/Tours.js
+++ src/Tours.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Tour from "./Tour";
 
-const Tours = () => {
+const Tours = ({ tours }) => {
   return (
-    <div>
-      <h2>tours component</h2>
-    </div>
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return (
+            <Tour key={tour.id} {...tour}>
+              {" "}
+            </Tour>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
