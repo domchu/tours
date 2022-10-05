919ef8ca185241fe8d1c8157bc0f848f150d53b6
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-05 23:59:39 +0100

    chore:added the refresh button

--- src/App.js
+++ src/App.js
@@ -54,6 +54,20 @@ function App() {
     );
   }
 
+  //function to return all the tour
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
